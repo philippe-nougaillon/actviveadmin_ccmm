3a70b4d886dcbe19232b2d6388e0d797df80b21f
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2026-05-18 17:44:59 +0200

    gemfile reordered & adminuser audited

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,8 @@
 class AdminUser < ApplicationRecord
   DEFAULT_EMAIL = "admin@example.com"
 
+  audited
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable
